ae89036bbe7e31f04d5db01bcf9b7f4401311750
Dick Olsson <dick.olsson@senzilla.com>
2014-11-16 15:57:37 +0100

    First stab at removing all code related to replication

--- src/Plugin/Field/FieldType/ReplicationHistoryItemList.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\relaxed\Plugin\Field\FieldType;
-
-use Drupal\Core\Field\FieldItemList;
-
-class ReplicationHistoryItemList extends FieldItemList {
-  // @todo Ensure we always have at least one item in this list.
-}
